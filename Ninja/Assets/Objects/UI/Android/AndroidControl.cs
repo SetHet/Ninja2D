1937d2d9d004f23bc0a725cd2a3745a6ad331653
SetHet <36393893+SetHet@users.noreply.github.com>
2019-04-20 18:46:18 -0400

    control android

---
+++ Ninja/Assets/Objects/UI/Android/AndroidControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidControl : MonoBehaviour
+{
+    #region UI
+    public RectTransform stick_limits;
+    public RectTransform stick_area;
+    public RectTransform stick_point;
+
+    public Button attack;
+    public Button jump;
+    #endregion
+    private void Start()
+    {
+        if (!OnlyAndroid()) return;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    bool OnlyAndroid()
+    {
+#if !(UNITY_ANDROID || UNITY_EDITOR)
+            Destroy(gameObject);
+            return false;
+#endif
+        return true;
+    }
+}
